d7e8512c6ca97312235ca1cafec7a278c635cb8c
Mahammad Jan-E-Alam <janealam416@gmail.com>
2016-10-28 00:54:54 +0600

    First Commit for plugin main file.

---
+++ developer-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Developer-Plugin
+ * Plugin URI: http://it.skilltouch.net
+ * Description: This plugin for custom functional work for this project. Don't Deactivate or Delete this plugin. It may break website or some pages.
+ * Version: 1.0.0
+ * Author: Mahammad Jan-E-Alam
+ * Author URI: http://www.janealam.com
+ * License: GPL2
+ */
